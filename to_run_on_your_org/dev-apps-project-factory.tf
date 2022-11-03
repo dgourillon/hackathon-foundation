dcb376f3b28ee751c9abda21470e9cc005714670
Didier Gourillon <dgourillon@google.com>
2022-11-03 17:34:46 +0100

    fix the apps_folder

--- to_run_on_your_org/dev-apps-project-factory.tf
+++ to_run_on_your_org/dev-apps-project-factory.tf
@@ -39,7 +39,7 @@ module "projects" {
   billing_alert          = try(each.value.billing_alert, null)
   dns_zones              = try(each.value.dns_zones, [])
   essential_contacts     = try(each.value.essential_contacts, [])
-  folder_id              = each.value.folder_id
+  folder_id              = module.apps_folder.id
   group_iam              = try(each.value.group_iam, {})
   iam                    = try(each.value.iam, {})
   kms_service_agents     = try(each.value.kms, {})
