2003e834b50210d9aa9c9c542717017aea574fab
Didier Gourillon <dgourillon@google.com>
2022-11-04 13:51:00 +0100

    update the provider to the exmpale found on the terraform ws

--- to_run_on_your_org/providers.tf
+++ to_run_on_your_org/providers.tf
@@ -1,26 +1,23 @@
 
-
-data "google_service_account_access_token" "sa" {
-  
-  target_service_account = "terraform-sa@hackathon-admin-367612.iam.gserviceaccount.com"
-  lifetime               = "600s"
-scopes = [
-    "https://www.googleapis.com/auth/cloud-platform",
-  ]
-}
-/******************************************
-  GA Provider configuration
- *****************************************/
 provider "google" {
-  
-  access_token = data.google_service_account_access_token.sa.access_token
-
 }
-/******************************************
-  Beta Provider configuration
- *****************************************/
-provider "google-beta" {
-  
-  access_token = data.google_service_account_access_token.sa.access_token
 
+data "google_client_config" "default" {
+  provider = google
 }
+
+data "google_service_account_access_token" "default" {
+  provider               = google
+  target_service_account = "terraform-sa@hackathon-admin-367612.iam.gserviceaccount.com"
+  scopes                 = ["userinfo-email", "cloud-platform"]
+  lifetime               = "300s"
+}
+
+provider "google" {
+  alias        = "impersonated"
+  access_token = data.google_service_account_access_token.default.access_token
+}
+
+
+
+
