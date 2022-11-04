f781562bf68f63fabb7da7050caa3580cea844ea
Didier Gourillon <dgourillon@google.com>
2022-11-04 11:19:51 +0100

    dd

--- to_run_on_your_org/dev-apps-project-factory.tf
+++ to_run_on_your_org/dev-apps-project-factory.tf
@@ -28,8 +28,8 @@ locals {
   }
   defaults = merge(local._defaults, local._defaults_net)
   projects = {
-    for f in fileset("${var.data_dir}", "**/*.yaml") :
-    trimsuffix(f, ".yaml") => yamldecode(file("${var.data_dir}/${f}"))
+    for f in fileset("${var.data_dir_projects}", "**/*.yaml") :
+    trimsuffix(f, ".yaml") => yamldecode(file("${var.data_dir_projects}/${f}"))
   }
 }
 
