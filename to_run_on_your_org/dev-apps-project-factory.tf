de429a58e971d04170d9caf688effe00cde6e7fe
Didier Gourillon <dgourillon@google.com>
2022-11-04 11:43:33 +0100

    sdfn,

--- to_run_on_your_org/dev-apps-project-factory.tf
+++ to_run_on_your_org/dev-apps-project-factory.tf
@@ -38,7 +38,7 @@ module "projects" {
   source                 = "./factories/project-factory"
   for_each               = local.projects
   defaults               = local.defaults
-  project_id             = replace("${each.key}-${random_string.random.result}", "projects/", "-")
+  project_id             = replace("${each.key}-${random_string.random.result}", "projects/", "")
   billing_account_id     = var.billing_account
   billing_alert          = try(each.value.billing_alert, null)
  # dns_zones              = try(each.value.dns_zones, [])
