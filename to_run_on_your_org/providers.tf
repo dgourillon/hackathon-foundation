c2b82ab89298a8967761471a7402b4d3345cdf18
Didier Gourillon <dgourillon@google.com>
2022-11-11 15:57:38 +0100

    fix provider

--- to_run_on_your_org/providers.tf
+++ to_run_on_your_org/providers.tf
@@ -1,13 +1,13 @@
 
 provider "google" {
 
-  impersonate_service_account = " terraform-sa@hackathon-admin-367612.iam.gserviceaccount.com"
+  impersonate_service_account = "terraform-sa@hackathon-admin-367612.iam.gserviceaccount.com"
 
 }
 
 provider "google-beta" {
 
-  impersonate_service_account = " terraform-sa@hackathon-admin-367612.iam.gserviceaccount.com"
+  impersonate_service_account = "terraform-sa@hackathon-admin-367612.iam.gserviceaccount.com"
 
 }
 
