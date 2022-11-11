0d0defdce611b1d678b566919a9309fbc6a4f608
Didier Gourillon <dgourillon@google.com>
2022-11-11 22:04:23 +0100

    enable vpc on dev project

--- to_run_on_your_org/projects-apps.tf
+++ to_run_on_your_org/projects-apps.tf
@@ -55,6 +55,6 @@ module "dev-projects" {
   service_accounts_iam   = try(each.value.service_accounts_iam, {})
   services               = try(each.value.services, [])
   service_identities_iam = try(each.value.service_identities_iam, {})
-  vpc                    = try(each.value.vpc, null)
+  vpc                    = module.project_network_spoke_dev.project_id
 }
 
