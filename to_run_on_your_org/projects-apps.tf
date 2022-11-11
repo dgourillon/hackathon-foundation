6ee225b3fc7a816642f79e7481e260100865dee4
Didier Gourillon <dgourillon@google.com>
2022-11-11 15:26:14 +0100

    deleted:    to_run_on_your_org/data/cidrs.yaml 	deleted:    to_run_on_your_org/data/dashboards/firewall_insights.json 	deleted:    to_run_on_your_org/data/dashboards/vpn.json 	deleted:    to_run_on_your_org/data/firewall-rules/dev/rules.yaml 	deleted:    to_run_on_your_org/data/firewall-rules/landing/rules.yaml 	deleted:    to_run_on_your_org/data/hierarchical-policy-rules.yaml deleted:    to_run_on_your_org/data/subnets/dev/dev-dataplatform-ew1.yaml 	deleted:    to_run_on_your_org/data/subnets/dev/dev-default-ew1.yaml 	deleted:    to_run_on_your_org/data/subnets/dev/dev-gke-nodes-ew1.yaml 	deleted:    to_run_on_your_org/data/subnets/landing/landing-default-ew1.yaml 	deleted:    to_run_on_your_org/data/subnets/prod/prod-default-ew1.yaml modified:   to_run_on_your_org/network-landing.tf 	modified:   to_run_on_your_org/network-spoke-dev.tf 	new file:   to_run_on_your_org/network-spoke-prod.tf 	modified:   to_run_on_your_org/projects-apps.tf

--- to_run_on_your_org/projects-apps.tf
+++ to_run_on_your_org/projects-apps.tf
@@ -27,8 +27,8 @@ locals {
 
   }
   defaults = merge(local._defaults, local._defaults_net)
-  projects = {
-    for f in fileset("${var.data_dir_projects}", "**/*.yaml") :
+  dev_projects = {
+    for f in fileset("${var.data_dir_projects}", "dev/*.yaml") :
     trimsuffix(f, ".yaml") => yamldecode(file("${var.data_dir_projects}/${f}"))
   }
 }
@@ -36,9 +36,9 @@ locals {
 
 module "projects" {
   source                 = "./factories/project-factory"
-  for_each               = local.projects
+  for_each               = local.dev_projects
   defaults               = local.defaults
-  project_id             = replace("${each.key}-${random_string.random.result}", "projects/", "")
+  project_id             = replace("${each.key}-${random_string.random.result}", "dev/", "")
   billing_account_id     = var.billing_account
   billing_alert          = try(each.value.billing_alert, null)
  # dns_zones              = try(each.value.dns_zones, [])
@@ -57,5 +57,4 @@ module "projects" {
   service_identities_iam = try(each.value.service_identities_iam, {})
   vpc                    = try(each.value.vpc, null)
 }
-
 
