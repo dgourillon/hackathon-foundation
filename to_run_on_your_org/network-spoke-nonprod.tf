1b8ce2d0b2939ce842064133b72aa8ba45f96d61
Didier Gourillon <dgourillon@google.com>
2022-11-15 01:52:26 +0100

    switch iap to all instanc ei n net

--- to_run_on_your_org/network-spoke-nonprod.tf
+++ to_run_on_your_org/network-spoke-nonprod.tf
@@ -64,7 +64,7 @@ module "nonprod-spoke-firewall" {
     allow-iap = {
       description   = "Allow IAP on SSH and RDP"
       source_ranges = ["35.235.240.0/20"]
-      targets       = ["allow-iap"]
+
       rules         = [{ protocol = "tcp", ports = ["22","3389"] }]
     }
   }
