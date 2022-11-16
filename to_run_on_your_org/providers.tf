0e75a7677fce954b5a4ac2e23176ad64358d8b80
Didier Gourillon <dgourillon@google.com>
2022-11-16 12:02:16 +0100

    set the provider bversion to 4.40.0

--- to_run_on_your_org/providers.tf
+++ to_run_on_your_org/providers.tf
@@ -2,13 +2,14 @@
 provider "google" {
 
   impersonate_service_account = "terraform-sa@hackathon-admin-367612.iam.gserviceaccount.com"
+  version                     = "4.40.0"
 
 }
 
 provider "google-beta" {
 
   impersonate_service_account = "terraform-sa@hackathon-admin-367612.iam.gserviceaccount.com"
-
+  version                     = "4.40.0"
 }
 
 
